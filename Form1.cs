33cfa3c7f9fbb170a0c4983f22ee564679c10cbe
itsTheFlex Olvera <relasedmoney72@outlook.com>
2026-09-08 08:04:19 -0600

    Ahora el programa, anota el proceso en un archivo de texto y desde ahi retoma las juagdas

--- Form1.cs
+++ Form1.cs
@@ -1,74 +1,84 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PIEDRA
 {
     public partial class Form1 : Form
     {
-        // Matriz de transición de Markov
+        // 0 = Piedra, 1 = Papel, 2 = Tijera
         int[,] matriz = new int[3, 3];
 
-
         int ultimaEleccion = -1;
+        int jugadasAprendizaje = 0;
+
+        const int LIMITE_APRENDIZAJE = 100;
 
         Random random = new Random();
 
-        // Vector que guarda cada jugada
-        List<(int jugador, int computadora)> historial = new List<(int, int)>();
+        // Guarda las primeras 100 jugadas
+        List<(int jugador, int computadora)> historial =
+            new List<(int, int)>();
 
-        // Contadores de elecciones
-        int[] conteoJugador = new int[3];      
+        int[] conteoJugador = new int[3];
         int[] conteoComputadora = new int[3];
-        // Contadores de resultados del jugador
+
         int victorias = 0;
         int derrotas = 0;
         int empates = 0;
 
-        string rutaArchivo = "C:\\Users\\jujov\\OneDrive\\doc_lenguajes\\piedra, papel\\p,p,t.txt";
+        // El archivo se creará en la carpeta donde se ejecuta el programa
+        string rutaArchivo = Path.Combine(
+            Application.StartupPath,
+            "conocimiento_piedra_papel_tijera.txt"
+        );
 
         public Form1()
         {
             InitializeComponent();
+
+            // Recuperar lo aprendido anteriormente
+            cargarConocimiento();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ELECCION.Text =
+                "Jugadas aprendidas: " + jugadasAprendizaje +
+                " de " + LIMITE_APRENDIZAJE;
         }
 
         private void jugar(int eleccionJugador)
         {
+            ELECCION.Text =
+                "Tu elección fue " + nombreEleccion(eleccionJugador);
 
-
-            // Guardar la elección del jugador
-            ELECCION.Text = "Tu elección fue " + nombreEleccion(eleccionJugador);
-
-
-            if (ultimaEleccion != -1)
+            /*
+             * Solamente se modifica la matriz durante las
+             * primeras 100 jugadas.
+             */
+            if (jugadasAprendizaje < LIMITE_APRENDIZAJE)
             {
-                matriz[ultimaEleccion, eleccionJugador]++;
+                if (ultimaEleccion != -1)
+                {
+                    matriz[ultimaEleccion, eleccionJugador]++;
+                }
             }
 
-            // La computadora elige usando Markov
+            // La computadora utiliza la matriz aprendida
             int eleccionComputadora = elegirMarkov();
 
+            ELECCION.Text +=
+                "\r\nLa computadora eligió " +
+                nombreEleccion(eleccionComputadora);
 
-            ELECCION.Text += "\r\nLa computadora eligió "
-                + nombreEleccion(eleccionComputadora);
-
+            bool jugadorGano = false;
+            bool computadoraGano = false;
 
             if (eleccionJugador == eleccionComputadora)
             {
                 ELECCION.Text += "\r\nEmpate";
-                empates++;
             }
             else if (
                 (eleccionJugador == 0 && eleccionComputadora == 2) ||
@@ -77,75 +87,80 @@ namespace PIEDRA
             )
             {
                 ELECCION.Text += "\r\n¡Ganaste!";
-                victorias++;
+                jugadorGano = true;
             }
             else
             {
                 ELECCION.Text += "\r\nGanó la computadora";
-                derrotas++;
+                computadoraGano = true;
             }
-            ultimaEleccion = eleccionJugador;
 
-            // Guardar la jugada en el historial 
-            historial.Add((eleccionJugador, eleccionComputadora));
-            conteoJugador[eleccionJugador]++;
-            conteoComputadora[eleccionComputadora]++;
-            guardarResultados();
-        }
-
-        private void guardarResultados()
-        {
-            using (StreamWriter writer = new StreamWriter(rutaArchivo, false))
+            /*
+             * Guardar información únicamente mientras
+             * todavía se encuentra aprendiendo.
+             */
+            if (jugadasAprendizaje < LIMITE_APRENDIZAJE)
             {
-                writer.WriteLine("=== HISTORIAL DE JUGADAS ===");
-                for (int i = 0; i < historial.Count; i++)
+                historial.Add(
+                    (eleccionJugador, eleccionComputadora)
+                );
+
+                conteoJugador[eleccionJugador]++;
+                conteoComputadora[eleccionComputadora]++;
+
+                if (jugadorGano)
                 {
-                    writer.WriteLine(
-                        $"Ronda {i + 1}: Jugador eligió {nombreEleccion(historial[i].jugador)} "
-                        + $"- Computadora eligió {nombreEleccion(historial[i].computadora)}"
-                    );
+                    victorias++;
+                }
+                else if (computadoraGano)
+                {
+                    derrotas++;
+                }
+                else
+                {
+                    empates++;
                 }
 
+                jugadasAprendizaje++;
 
-                writer.WriteLine();
-                writer.WriteLine("=== CONTEO DE ELECCIONES ===");
-                writer.WriteLine("Jugador:");
-                writer.WriteLine($"  Piedra: {conteoJugador[0]}");
-                writer.WriteLine($"  Papel: {conteoJugador[1]}");
-                writer.WriteLine($"  Tijera: {conteoJugador[2]}");
+                /*
+                 * Se guarda la jugada actual para que, si se
+                 * cierra el programa, continúe correctamente.
+                 */
+                ultimaEleccion = eleccionJugador;
 
-                writer.WriteLine("Computadora:");
-                writer.WriteLine($"  Piedra: {conteoComputadora[0]}");
-                writer.WriteLine($"  Papel: {conteoComputadora[1]}");
-                writer.WriteLine($"  Tijera: {conteoComputadora[2]}");
-
-                writer.WriteLine();
-                writer.WriteLine("=== RESULTADOS DEL JUGADOR ===");
-                writer.WriteLine($"  Victorias: {victorias}");
-                writer.WriteLine($"  Derrotas: {derrotas}");
-                writer.WriteLine($"  Empates: {empates}");
+                guardarConocimiento();
             }
-        }
-        private string nombreEleccion(int eleccion)
-        {
-            if (eleccion == 0)
-                return "Piedra";
+            else
+            {
+                /*
+                 * Después de 100 jugadas se actualiza solamente
+                 * para saber cuál fue la jugada anterior.
+                 * La matriz ya no cambia.
+                 */
+                ultimaEleccion = eleccionJugador;
+            }
 
-            if (eleccion == 1)
-                return "Papel";
+            ELECCION.Text +=
+                "\r\nJugadas aprendidas: " +
+                jugadasAprendizaje + " de " +
+                LIMITE_APRENDIZAJE;
 
-            return "Tijera";
+            if (jugadasAprendizaje >= LIMITE_APRENDIZAJE)
+            {
+                ELECCION.Text +=
+                    "\r\nAprendizaje terminado. Ahora utiliza " +
+                    "el conocimiento guardado.";
+            }
         }
 
         private int elegirMarkov()
         {
-            // Si no tenemos una jugada anterior
+            // Si todavía no existe una jugada anterior
             if (ultimaEleccion == -1)
             {
                 return random.Next(0, 3);
             }
-
-
 
             int piedra = matriz[ultimaEleccion, 0];
             int papel = matriz[ultimaEleccion, 1];
@@ -153,61 +168,315 @@ namespace PIEDRA
 
             int total = piedra + papel + tijera;
 
-            // Si no hay información suficiente
+            // Si no hay información para esa jugada
             if (total == 0)
             {
                 return random.Next(0, 3);
             }
 
-            // Elegimos cuál es la jugada más probable del jugador
             int prediccion;
 
+            // Se busca qué movimiento es el más probable
             if (piedra >= papel && piedra >= tijera)
             {
-                prediccion = 0; 
+                prediccion = 0;
             }
             else if (papel >= piedra && papel >= tijera)
             {
-                prediccion = 1; 
+                prediccion = 1;
             }
             else
             {
-                prediccion = 2; 
+                prediccion = 2;
             }
 
-            // Elegimos la jugada que vence la predicción
+            // La computadora selecciona lo que vence la predicción
             if (prediccion == 0)
             {
-                return 1; 
+                return 1; // Papel vence a Piedra
             }
             else if (prediccion == 1)
             {
-                return 2; 
+                return 2; // Tijera vence a Papel
             }
             else
             {
-                return 0; 
+                return 0; // Piedra vence a Tijera
             }
+        }
+
+        private void guardarConocimiento()
+        {
+            try
+            {
+                using (StreamWriter writer =
+                    new StreamWriter(rutaArchivo, false))
+                {
+                    /*
+                     * Esta parte contiene la información que
+                     * el programa necesita para continuar.
+                     */
+                    writer.WriteLine("DATOS_DEL_PROGRAMA");
+                    writer.WriteLine(
+                        "Jugadas=" + jugadasAprendizaje
+                    );
+                    writer.WriteLine(
+                        "UltimaEleccion=" + ultimaEleccion
+                    );
+
+                    writer.WriteLine(
+                        "ConteoJugador=" +
+                        conteoJugador[0] + "," +
+                        conteoJugador[1] + "," +
+                        conteoJugador[2]
+                    );
+
+                    writer.WriteLine(
+                        "ConteoComputadora=" +
+                        conteoComputadora[0] + "," +
+                        conteoComputadora[1] + "," +
+                        conteoComputadora[2]
+                    );
+
+                    writer.WriteLine("Victorias=" + victorias);
+                    writer.WriteLine("Derrotas=" + derrotas);
+                    writer.WriteLine("Empates=" + empates);
+
+                    // Guardar la matriz de Markov
+                    writer.WriteLine("Matriz0=" +
+                        matriz[0, 0] + "," +
+                        matriz[0, 1] + "," +
+                        matriz[0, 2]);
+
+                    writer.WriteLine("Matriz1=" +
+                        matriz[1, 0] + "," +
+                        matriz[1, 1] + "," +
+                        matriz[1, 2]);
+
+                    writer.WriteLine("Matriz2=" +
+                        matriz[2, 0] + "," +
+                        matriz[2, 1] + "," +
+                        matriz[2, 2]);
+
+                    writer.WriteLine();
+                    writer.WriteLine("HISTORIAL");
+
+                    for (int i = 0; i < historial.Count; i++)
+                    {
+                        writer.WriteLine(
+                            (i + 1) + "," +
+                            historial[i].jugador + "," +
+                            historial[i].computadora
+                        );
+                    }
+
+                    writer.WriteLine();
+                    writer.WriteLine(
+                        "SIGNIFICADO: 0=Piedra, 1=Papel, 2=Tijera"
+                    );
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(
+                    "No se pudo guardar el archivo:\n" +
+                    error.Message
+                );
+            }
+        }
+
+        private void cargarConocimiento()
+        {
+            // Si todavía no existe, comienza desde cero
+            if (!File.Exists(rutaArchivo))
+            {
+                return;
+            }
+
+            try
+            {
+                string[] lineas = File.ReadAllLines(rutaArchivo);
+
+                historial.Clear();
+
+                bool leyendoHistorial = false;
+
+                foreach (string lineaOriginal in lineas)
+                {
+                    string linea = lineaOriginal.Trim();
+
+                    if (linea == "HISTORIAL")
+                    {
+                        leyendoHistorial = true;
+                        continue;
+                    }
+
+                    if (linea.StartsWith("SIGNIFICADO"))
+                    {
+                        leyendoHistorial = false;
+                        continue;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(linea))
+                    {
+                        continue;
+                    }
+
+                    if (leyendoHistorial)
+                    {
+                        cargarJugadaHistorial(linea);
+                    }
+                    else
+                    {
+                        cargarDato(linea);
+                    }
+                }
+
+                // Seguridad para que nunca pase de 100
+                if (jugadasAprendizaje > LIMITE_APRENDIZAJE)
+                {
+                    jugadasAprendizaje = LIMITE_APRENDIZAJE;
+                }
+
+                MessageBox.Show(
+                    "Conocimiento recuperado correctamente.\n" +
+                    "Jugadas aprendidas: " +
+                    jugadasAprendizaje + " de " +
+                    LIMITE_APRENDIZAJE
+                );
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(
+                    "No se pudo cargar el conocimiento:\n" +
+                    error.Message
+                );
+            }
+        }
+
+        private void cargarDato(string linea)
+        {
+            string[] partes = linea.Split('=');
+
+            if (partes.Length != 2)
+            {
+                return;
+            }
+
+            string nombre = partes[0];
+            string valor = partes[1];
+
+            switch (nombre)
+            {
+                case "Jugadas":
+                    jugadasAprendizaje = int.Parse(valor);
+                    break;
+
+                case "UltimaEleccion":
+                    ultimaEleccion = int.Parse(valor);
+                    break;
+
+                case "ConteoJugador":
+                    cargarVector(valor, conteoJugador);
+                    break;
+
+                case "ConteoComputadora":
+                    cargarVector(valor, conteoComputadora);
+                    break;
+
+                case "Victorias":
+                    victorias = int.Parse(valor);
+                    break;
+
+                case "Derrotas":
+                    derrotas = int.Parse(valor);
+                    break;
+
+                case "Empates":
+                    empates = int.Parse(valor);
+                    break;
+
+                case "Matriz0":
+                    cargarFilaMatriz(0, valor);
+                    break;
+
+                case "Matriz1":
+                    cargarFilaMatriz(1, valor);
+                    break;
+
+                case "Matriz2":
+                    cargarFilaMatriz(2, valor);
+                    break;
+            }
+        }
+
+        private void cargarVector(string texto, int[] vector)
+        {
+            string[] numeros = texto.Split(',');
+
+            if (numeros.Length == 3)
+            {
+                vector[0] = int.Parse(numeros[0]);
+                vector[1] = int.Parse(numeros[1]);
+                vector[2] = int.Parse(numeros[2]);
+            }
+        }
+
+        private void cargarFilaMatriz(int fila, string texto)
+        {
+            string[] numeros = texto.Split(',');
+
+            if (numeros.Length == 3)
+            {
+                matriz[fila, 0] = int.Parse(numeros[0]);
+                matriz[fila, 1] = int.Parse(numeros[1]);
+                matriz[fila, 2] = int.Parse(numeros[2]);
+            }
+        }
+
+        private void cargarJugadaHistorial(string linea)
+        {
+            string[] datos = linea.Split(',');
+
+            if (datos.Length != 3)
+            {
+                return;
+            }
+
+            int jugador = int.Parse(datos[1]);
+            int computadora = int.Parse(datos[2]);
+
+            historial.Add((jugador, computadora));
+        }
+
+        private string nombreEleccion(int eleccion)
+        {
+            if (eleccion == 0)
+            {
+                return "Piedra";
+            }
+
+            if (eleccion == 1)
+            {
+                return "Papel";
+            }
+
+            return "Tijera";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
             jugar(0);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
             jugar(1);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
             jugar(2);
         }
     }
 }
-
-
