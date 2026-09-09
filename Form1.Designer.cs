68caabe00da8fbff2dd52b66e8091643fd83eeed
rafa_ <rafa_@LAPTOP-4URDQLDB>
2026-09-09 08:08:00 -0600

    Disenio del piedra y aumentoi codigo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,103 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.ELECCION = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoEllipsis = true;
-            this.label1.AutoSize = true;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(56, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(431, 41);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "PIEDRA,PAPEL O TIJERA";
             // 
             // ELECCION
             // 
             this.ELECCION.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.ELECCION.AutoSize = true;
-            this.ELECCION.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.ELECCION.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ELECCION.Location = new System.Drawing.Point(151, 374);
+            this.ELECCION.BackColor = System.Drawing.Color.Transparent;
+            this.ELECCION.Font = new System.Drawing.Font("LEGO BRIX", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ELECCION.Image = global::PIEDRA.Properties.Resources.WhatsApp_Image_2026_09_03_at_6_31_45_PM;
+            this.ELECCION.Location = new System.Drawing.Point(135, 500);
             this.ELECCION.Name = "ELECCION";
-            this.ELECCION.Size = new System.Drawing.Size(176, 31);
+            this.ELECCION.Size = new System.Drawing.Size(395, 137);
             this.ELECCION.TabIndex = 1;
-            this.ELECCION.Text = "TU ELECCION";
+            this.ELECCION.Text = "            TU ELCCION";
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackgroundImage = global::PIEDRA.Properties.Resources.WhatsApp_Image_2026_09_03_at_6_311;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(151, 73);
+            this.button1.Location = new System.Drawing.Point(135, 180);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(250, 75);
+            this.button1.Size = new System.Drawing.Size(400, 60);
             this.button1.TabIndex = 2;
-            this.button1.Text = "PIEDRA";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.boton_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.boton_MouseUp);
             // 
             // button2
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackgroundImage = global::PIEDRA.Properties.Resources.piedra;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(151, 169);
+            this.button2.Location = new System.Drawing.Point(135, 280);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(250, 75);
+            this.button2.Size = new System.Drawing.Size(400, 60);
             this.button2.TabIndex = 3;
-            this.button2.Text = "PAPEL";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.boton_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.boton_MouseUp);
             // 
             // button3
             // 
             this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.BackgroundImage = global::PIEDRA.Properties.Resources.Gemini_Generated_Image_pbusl9pbusl9pbus;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(151, 260);
+            this.button3.Location = new System.Drawing.Point(135, 380);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(250, 75);
+            this.button3.Size = new System.Drawing.Size(400, 60);
             this.button3.TabIndex = 4;
-            this.button3.Text = "TIJERAS";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.boton_MouseDown);
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.boton_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.BackgroundImage = global::PIEDRA.Properties.Resources.ChatGPT_Image_8_sept_2026__04_30_41_p_m_;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(65, 45);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(520, 70);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.BackgroundImage = global::PIEDRA.Properties.Resources.minecra;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(532, 503);
+            this.ClientSize = new System.Drawing.Size(647, 685);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ELECCION);
-            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("LEGO BRIX", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.HelpButton = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label ELECCION;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        public System.Windows.Forms.Label ELECCION;
     }
 }
 
