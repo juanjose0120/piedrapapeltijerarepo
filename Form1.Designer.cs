405243ff1b50f53c6e10016ab87cf7e2626c9ea4
itsTheFlex Olvera <relasedmoney72@outlook.com>
2026-09-14 13:57:28 -0600

    Se agrego imagenes cuando el usuario elije piedra, papel o tijera

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,19 +33,24 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.pictureMaquina = new System.Windows.Forms.PictureBox();
+            this.pictureUsuario = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMaquina)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUsuario)).BeginInit();
             this.SuspendLayout();
             // 
             // ELECCION
             // 
             this.ELECCION.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.ELECCION.BackColor = System.Drawing.Color.Transparent;
-            this.ELECCION.Font = new System.Drawing.Font("LEGO BRIX", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ELECCION.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ELECCION.Image = global::PIEDRA.Properties.Resources.WhatsApp_Image_2026_09_03_at_6_31_45_PM;
-            this.ELECCION.Location = new System.Drawing.Point(135, 500);
+            this.ELECCION.Location = new System.Drawing.Point(251, 505);
             this.ELECCION.Name = "ELECCION";
             this.ELECCION.Size = new System.Drawing.Size(395, 137);
             this.ELECCION.TabIndex = 1;
-            this.ELECCION.Text = "            TU ELCCION";
+            this.ELECCION.Text = "            TU ELECCION";
+            this.ELECCION.Click += new System.EventHandler(this.ELECCION_Click);
             // 
             // button1
             // 
@@ -57,7 +62,7 @@
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(135, 180);
+            this.button1.Location = new System.Drawing.Point(251, 185);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(400, 60);
             this.button1.TabIndex = 2;
@@ -74,7 +79,7 @@
             this.button2.FlatAppearance.BorderSize = 0;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(135, 280);
+            this.button2.Location = new System.Drawing.Point(251, 285);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(400, 60);
             this.button2.TabIndex = 3;
@@ -91,7 +96,7 @@
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(135, 380);
+            this.button3.Location = new System.Drawing.Point(251, 385);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(400, 60);
             this.button3.TabIndex = 4;
@@ -107,33 +112,58 @@
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.Location = new System.Drawing.Point(65, 45);
+            this.button4.Location = new System.Drawing.Point(200, 50);
             this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(520, 70);
             this.button4.TabIndex = 5;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureMaquina
+            // 
+            this.pictureMaquina.Location = new System.Drawing.Point(33, 269);
+            this.pictureMaquina.Name = "pictureMaquina";
+            this.pictureMaquina.Size = new System.Drawing.Size(160, 185);
+            this.pictureMaquina.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureMaquina.TabIndex = 6;
+            this.pictureMaquina.TabStop = false;
+            this.pictureMaquina.Click += new System.EventHandler(this.pictureMaquina_Click);
+            // 
+            // pictureUsuario
+            // 
+            this.pictureUsuario.Location = new System.Drawing.Point(706, 269);
+            this.pictureUsuario.Name = "pictureUsuario";
+            this.pictureUsuario.Size = new System.Drawing.Size(160, 185);
+            this.pictureUsuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureUsuario.TabIndex = 7;
+            this.pictureUsuario.TabStop = false;
+            this.pictureUsuario.Click += new System.EventHandler(this.pictureUsuario_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.BackgroundImage = global::PIEDRA.Properties.Resources.minecra;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(647, 685);
+            this.ClientSize = new System.Drawing.Size(878, 694);
+            this.Controls.Add(this.pictureUsuario);
+            this.Controls.Add(this.pictureMaquina);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ELECCION);
-            this.Font = new System.Drawing.Font("LEGO BRIX", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.HelpButton = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Piedra, Papel o Tijera";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureMaquina)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUsuario)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -144,6 +174,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         public System.Windows.Forms.Label ELECCION;
+        private System.Windows.Forms.PictureBox pictureMaquina;
+        private System.Windows.Forms.PictureBox pictureUsuario;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,55 +9,134 @@ namespace PIEDRA
     {
         // 0 = Piedra, 1 = Papel, 2 = Tijera
         int[,] matriz = new int[3, 3];
-
         int ultimaEleccion = -1;
         int jugadasAprendizaje = 0;
-
         const int LIMITE_APRENDIZAJE = 100;
-
         Random random = new Random();
-
         // Guarda las primeras 100 jugadas
+        // Tamaño en el que diseñaste originalmente la ventana
+        int anchoOriginal = 897;
+        int altoOriginal = 761;
         List<(int jugador, int computadora)> historial =
             new List<(int, int)>();
-
         int[] conteoJugador = new int[3];
         int[] conteoComputadora = new int[3];
-
         int victorias = 0;
         int derrotas = 0;
         int empates = 0;
-
         // El archivo se creará en la carpeta donde se ejecuta el programa
         string rutaArchivo = Path.Combine(
             Application.StartupPath,
             "conocimiento_piedra_papel_tijera.txt"
         );
-
         public Form1()
         {
             InitializeComponent();
+            // Hace que las imágenes no se deformen ni se corten
+            pictureUsuario.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureMaquina.SizeMode = PictureBoxSizeMode.Zoom;
 
+            // Quita el fondo gris de los PictureBox
+            pictureUsuario.BackColor = System.Drawing.Color.Transparent;
+            pictureMaquina.BackColor = System.Drawing.Color.Transparent;
+
+            // Detectar cuando cambia el tamaño de la ventana
+            this.Resize += Form1_Resize;
             // Recuperar lo aprendido anteriormente
             cargarConocimiento();
         }
+        private void ajustarControles()
+        {
+            float escalaX = (float)this.ClientSize.Width / anchoOriginal;
+            float escalaY = (float)this.ClientSize.Height / altoOriginal;
+            // Imagen del usuario - izquierda
+            pictureUsuario.Location = new System.Drawing.Point(
+                (int)(41 * escalaX),
+                (int)(328 * escalaY)
+            );
+            pictureUsuario.Size = new System.Drawing.Size(
+                (int)(160 * escalaX),
+                (int)(186 * escalaY)
+            );
+            // Imagen de la computadora - derecha
+            pictureMaquina.Location = new System.Drawing.Point(
+                (int)(712 * escalaX),
+                (int)(326 * escalaY)
+            );
+            pictureMaquina.Size = new System.Drawing.Size(
+                (int)(164 * escalaX),
+                (int)(189 * escalaY)
+            );
+            // Botón Piedra
+            button1.Location = new System.Drawing.Point(
+                (int)(259 * escalaX),
+                (int)(244 * escalaY)
+            );
+            button1.Size = new System.Drawing.Size(
+                (int)(399 * escalaX),
+                (int)(60 * escalaY)
+            );
+            // Botón Papel
+            button2.Location = new System.Drawing.Point(
+                (int)(259 * escalaX),
+                (int)(344 * escalaY)
+            );
+            button2.Size = new System.Drawing.Size(
+                (int)(399 * escalaX),
+                (int)(60 * escalaY)
+            );
+            // Botón Tijera
+            button3.Location = new System.Drawing.Point(
+                (int)(259 * escalaX),
+                (int)(444 * escalaY)
+            );
+            button3.Size = new System.Drawing.Size(
+                (int)(399 * escalaX),
+                (int)(60 * escalaY)
+            );
+            // TextBox / Label donde aparece la elección
+            ELECCION.Location = new System.Drawing.Point(
+                (int)(259 * escalaX),
+                (int)(567 * escalaY)
+            );
+            ELECCION.Size = new System.Drawing.Size(
+                (int)(395 * escalaX),
+                (int)(129 * escalaY)
+            );
+            button4.Location = new System.Drawing.Point(
+                (int)(189 * escalaX),
+                (int)(109 * escalaY)
+            );
 
+            button4.Size = new System.Drawing.Size(
+                (int)(520 * escalaX),
+                (int)(70 * escalaY)
+            );
+            button4.Font = new System.Drawing.Font(
+                button4.Font.FontFamily,
+                28 * escalaY,
+                button4.Font.Style
+            );
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             ELECCION.Text =
                 "Jugadas aprendidas: " + jugadasAprendizaje +
                 " de " + LIMITE_APRENDIZAJE;
+            ajustarControles();
         }
-
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ajustarControles();
+        }
         private void jugar(int eleccionJugador)
         {
+            mostrarImagen(pictureUsuario, eleccionJugador);
             ELECCION.Text =
                 "Tu elección fue " + nombreEleccion(eleccionJugador);
-
-            /*
-             * Solamente se modifica la matriz durante las
-             * primeras 100 jugadas.
-             */
+            
+             /* Solamente se modifica la matriz durante las
+             * primeras 100 jugadas.*/
             if (jugadasAprendizaje < LIMITE_APRENDIZAJE)
             {
                 if (ultimaEleccion != -1)
@@ -65,17 +144,15 @@ namespace PIEDRA
                     matriz[ultimaEleccion, eleccionJugador]++;
                 }
             }
-
             // La computadora utiliza la matriz aprendida
             int eleccionComputadora = elegirMarkov();
+            mostrarImagen(pictureMaquina, eleccionComputadora);
 
             ELECCION.Text +=
                 "\r\nLa computadora eligió " +
                 nombreEleccion(eleccionComputadora);
-
             bool jugadorGano = false;
             bool computadoraGano = false;
-
             if (eleccionJugador == eleccionComputadora)
             {
                 ELECCION.Text += "\r\nEmpate";
@@ -94,11 +171,8 @@ namespace PIEDRA
                 ELECCION.Text += "\r\nGanó la computadora";
                 computadoraGano = true;
             }
-
-            /*
-             * Guardar información únicamente mientras
-             * todavía se encuentra aprendiendo.
-             */
+             /* Guardar información únicamente mientras
+             * todavía se encuentra aprendiendo.*/
             if (jugadasAprendizaje < LIMITE_APRENDIZAJE)
             {
                 historial.Add(
@@ -122,30 +196,22 @@ namespace PIEDRA
                 }
 
                 jugadasAprendizaje++;
-
-                /*
-                 * Se guarda la jugada actual para que, si se
-                 * cierra el programa, continúe correctamente.
-                 */
+                 /* Se guarda la jugada actual para que, si se
+                 * cierra el programa, continúe correctamente*/
                 ultimaEleccion = eleccionJugador;
-
                 guardarConocimiento();
             }
             else
             {
-                /*
-                 * Después de 100 jugadas se actualiza solamente
+                 /* Después de 100 jugadas se actualiza solamente
                  * para saber cuál fue la jugada anterior.
-                 * La matriz ya no cambia.
-                 */
+                 * La matriz ya no cambia.*/
                 ultimaEleccion = eleccionJugador;
             }
-
             ELECCION.Text +=
                 "\r\nJugadas aprendidas: " +
                 jugadasAprendizaje + " de " +
                 LIMITE_APRENDIZAJE;
-
             if (jugadasAprendizaje >= LIMITE_APRENDIZAJE)
             {
                 ELECCION.Text +=
@@ -153,7 +219,6 @@ namespace PIEDRA
                     "el conocimiento guardado.";
             }
         }
-
         private int elegirMarkov()
         {
             // Si todavía no existe una jugada anterior
@@ -161,21 +226,16 @@ namespace PIEDRA
             {
                 return random.Next(0, 3);
             }
-
             int piedra = matriz[ultimaEleccion, 0];
             int papel = matriz[ultimaEleccion, 1];
             int tijera = matriz[ultimaEleccion, 2];
-
             int total = piedra + papel + tijera;
-
             // Si no hay información para esa jugada
             if (total == 0)
             {
                 return random.Next(0, 3);
             }
-
             int prediccion;
-
             // Se busca qué movimiento es el más probable
             if (piedra >= papel && piedra >= tijera)
             {
@@ -204,7 +264,6 @@ namespace PIEDRA
                 return 0; // Piedra vence a Tijera
             }
         }
-
         private void guardarConocimiento()
         {
             try
@@ -212,10 +271,8 @@ namespace PIEDRA
                 using (StreamWriter writer =
                     new StreamWriter(rutaArchivo, false))
                 {
-                    /*
-                     * Esta parte contiene la información que
-                     * el programa necesita para continuar.
-                     */
+                     /* Esta parte contiene la información que
+                     * el programa necesita para continuar*/
                     writer.WriteLine("DATOS_DEL_PROGRAMA");
                     writer.WriteLine(
                         "Jugadas=" + jugadasAprendizaje
@@ -223,44 +280,36 @@ namespace PIEDRA
                     writer.WriteLine(
                         "UltimaEleccion=" + ultimaEleccion
                     );
-
                     writer.WriteLine(
                         "ConteoJugador=" +
                         conteoJugador[0] + "," +
                         conteoJugador[1] + "," +
                         conteoJugador[2]
                     );
-
                     writer.WriteLine(
                         "ConteoComputadora=" +
                         conteoComputadora[0] + "," +
                         conteoComputadora[1] + "," +
                         conteoComputadora[2]
                     );
-
                     writer.WriteLine("Victorias=" + victorias);
                     writer.WriteLine("Derrotas=" + derrotas);
                     writer.WriteLine("Empates=" + empates);
-
                     // Guardar la matriz de Markov
                     writer.WriteLine("Matriz0=" +
                         matriz[0, 0] + "," +
                         matriz[0, 1] + "," +
                         matriz[0, 2]);
-
                     writer.WriteLine("Matriz1=" +
                         matriz[1, 0] + "," +
                         matriz[1, 1] + "," +
                         matriz[1, 2]);
-
                     writer.WriteLine("Matriz2=" +
                         matriz[2, 0] + "," +
                         matriz[2, 1] + "," +
                         matriz[2, 2]);
-
                     writer.WriteLine();
                     writer.WriteLine("HISTORIAL");
-
                     for (int i = 0; i < historial.Count; i++)
                     {
                         writer.WriteLine(
@@ -269,7 +318,6 @@ namespace PIEDRA
                             historial[i].computadora
                         );
                     }
-
                     writer.WriteLine();
                     writer.WriteLine(
                         "SIGNIFICADO: 0=Piedra, 1=Papel, 2=Tijera"
@@ -284,7 +332,6 @@ namespace PIEDRA
                 );
             }
         }
-
         private void cargarConocimiento()
         {
             // Si todavía no existe, comienza desde cero
@@ -292,36 +339,28 @@ namespace PIEDRA
             {
                 return;
             }
-
             try
             {
                 string[] lineas = File.ReadAllLines(rutaArchivo);
-
                 historial.Clear();
-
                 bool leyendoHistorial = false;
-
                 foreach (string lineaOriginal in lineas)
                 {
                     string linea = lineaOriginal.Trim();
-
                     if (linea == "HISTORIAL")
                     {
                         leyendoHistorial = true;
                         continue;
                     }
-
                     if (linea.StartsWith("SIGNIFICADO"))
                     {
                         leyendoHistorial = false;
                         continue;
                     }
-
                     if (string.IsNullOrWhiteSpace(linea))
                     {
                         continue;
                     }
-
                     if (leyendoHistorial)
                     {
                         cargarJugadaHistorial(linea);
@@ -331,13 +370,11 @@ namespace PIEDRA
                         cargarDato(linea);
                     }
                 }
-
                 // Seguridad para que nunca pase de 100
                 if (jugadasAprendizaje > LIMITE_APRENDIZAJE)
                 {
                     jugadasAprendizaje = LIMITE_APRENDIZAJE;
                 }
-
                 MessageBox.Show(
                     "Conocimiento recuperado correctamente.\n" +
                     "Jugadas aprendidas: " +
@@ -353,7 +390,6 @@ namespace PIEDRA
                 );
             }
         }
-
         private void cargarDato(string linea)
         {
             string[] partes = linea.Split('=');
@@ -362,10 +398,8 @@ namespace PIEDRA
             {
                 return;
             }
-
             string nombre = partes[0];
             string valor = partes[1];
-
             switch (nombre)
             {
                 case "Jugadas":
@@ -409,7 +443,6 @@ namespace PIEDRA
                     break;
             }
         }
-
         private void cargarVector(string texto, int[] vector)
         {
             string[] numeros = texto.Split(',');
@@ -421,7 +454,6 @@ namespace PIEDRA
                 vector[2] = int.Parse(numeros[2]);
             }
         }
-
         private void cargarFilaMatriz(int fila, string texto)
         {
             string[] numeros = texto.Split(',');
@@ -433,53 +465,60 @@ namespace PIEDRA
                 matriz[fila, 2] = int.Parse(numeros[2]);
             }
         }
-
         private void cargarJugadaHistorial(string linea)
         {
             string[] datos = linea.Split(',');
-
             if (datos.Length != 3)
             {
                 return;
             }
-
             int jugador = int.Parse(datos[1]);
             int computadora = int.Parse(datos[2]);
 
             historial.Add((jugador, computadora));
         }
-
         private string nombreEleccion(int eleccion)
         {
             if (eleccion == 0)
             {
                 return "Piedra";
             }
-
             if (eleccion == 1)
             {
                 return "Papel";
             }
-
             return "Tijera";
         }
 
+        private void mostrarImagen(PictureBox picture, int eleccion)
+        {
+            picture.SizeMode = PictureBoxSizeMode.Zoom;
+            picture.BackColor = System.Drawing.Color.Transparent;
+            if (eleccion == 0)
+            {
+                picture.Image = Properties.Resources.Piedra_1;
+            }
+            else if (eleccion == 1)
+            {
+                picture.Image = Properties.Resources.papel_1;
+            }
+            else
+            {
+                picture.Image = Properties.Resources.Tijera_1;
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             jugar(0);
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             jugar(1);
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             jugar(2);
         }
-       
-
         private void boton_MouseDown(object sender, MouseEventArgs e)
         {
             Button boton = sender as Button;
@@ -489,7 +528,6 @@ namespace PIEDRA
                 boton.Left += 2;
             }
         }
-
         private void boton_MouseUp(object sender, MouseEventArgs e)
         {
             Button boton = sender as Button;
@@ -499,8 +537,27 @@ namespace PIEDRA
                 boton.Left -= 2;
             }
         }
-
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureMaquina_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureUsuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ELECCION_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
         {
 
         }
